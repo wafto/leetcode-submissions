92fe9d2a35e65ba18be3e007e28813fe7eddc2d0
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-01-15 03:32:34 +0000

    Code challenge submission for question: 283 Move Zeroes

---
+++ 283 Move Zeroes/283move-zeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left] = nums[right]
+                left += 1
+
+        while left < len(nums):
+            nums[left] = 0
+            left += 1 
+        

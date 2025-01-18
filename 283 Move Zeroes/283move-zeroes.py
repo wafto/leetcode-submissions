704ae081351c0e2150fcd6b598cd4a5b534e0743
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-01-18 05:51:20 +0000

    Code challenge submission for question: 283 Move Zeroes

--- 283 Move Zeroes/283move-zeroes.py
+++ 283 Move Zeroes/283move-zeroes.py
@@ -4,6 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         left = 0
+
         for right in range(len(nums)):
             if nums[right] != 0:
                 nums[left] = nums[right]
@@ -11,5 +12,4 @@ class Solution:
 
         while left < len(nums):
             nums[left] = 0
-            left += 1 
-        
+            left += 1

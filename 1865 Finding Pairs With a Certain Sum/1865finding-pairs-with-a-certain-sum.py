48a9c6dbd80cda1d6a6c2f3b1fdb47b26dab9a61
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-01-22 16:41:17 +0000

    Code challenge submission for question: 1865 Finding Pairs With a Certain Sum

---
+++ 1865 Finding Pairs With a Certain Sum/1865finding-pairs-with-a-certain-sum.py
@@ -0,0 +1,28 @@
+class FindSumPairs:
+
+    def __init__(self, nums1: List[int], nums2: List[int]):
+        self.nums1 = nums1
+        self.nums2 = nums2
+        
+        self.freq = defaultdict(int)
+        for x in nums2: self.freq[x] += 1
+
+    def add(self, index: int, val: int) -> None:
+        self.freq[self.nums2[index]] -= 1
+        self.nums2[index] += val
+        self.freq[self.nums2[index]] += 1
+
+    def count(self, tot: int) -> int:
+        ans = 0 
+        for x in self.nums1: 
+            ans += self.freq[tot - x]
+        return ans
+        
+
+        
+
+
+# Your FindSumPairs object will be instantiated and called as such:
+# obj = FindSumPairs(nums1, nums2)
+# obj.add(index,val)
+# param_2 = obj.count(tot)

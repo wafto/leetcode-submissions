1d2a32c56437173b725278bd8e15ec4f66126cd2
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-01-23 01:48:36 +0000

    Code challenge submission for question: 2130 Maximum Twin Sum of a Linked List

--- 2130 Maximum Twin Sum of a Linked List/2130maximum-twin-sum-of-a-linked-list.py
+++ 2130 Maximum Twin Sum of a Linked List/2130maximum-twin-sum-of-a-linked-list.py
@@ -15,31 +15,11 @@ class Solution:
             prev = slow
             slow = tmp
 
-        output = 0
+        maxtwin = 0
 
         while slow:
-            output = max(output, slow.val + prev.val)
+            maxtwin = max(maxtwin, slow.val + prev.val)
             slow = slow.next
             prev = prev.next
 
-        return output
-
-
-        #stack = []
-
-        #while fast and fast.next:
-        #    stack.append(slow.val)
-        #    fast = fast.next.next
-        #    slow = slow.next
-
-        #maxSum = -inf
-        
-        #while stack:
-        #    last = stack.pop()
-        #    maxSum = max(maxSum, last + slow.val)
-        #    slow = slow.next
-
-        #return maxSum
-
-
-
+        return maxtwin

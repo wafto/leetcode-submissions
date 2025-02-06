022b133f846a2b21599cfa0634a1fdbf90121ec7
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-02-06 23:45:45 +0000

    Code challenge submission for question: 1343 Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold

--- 1343 Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold/1343number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343 Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold/1343number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,12 +1,17 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        output = 0
-        curSum = sum(arr[:k - 1])
+        n = len(arr)
+        ps = [0] * (n + 1)
 
-        for i in range(len(arr) - k + 1):
-            curSum += arr[i + k - 1]
-            if (curSum / k) >= threshold:
-                output += 1
-            curSum -= arr[i]
+        prev = 0
+        for i in range(n):
+            prev += arr[i]
+            ps[i + 1] = prev
 
-        return output
+        left, ans = 0, 0
+        for right in range(k, len(ps)):
+            if (ps[right] - ps[left]) // k >= threshold:
+                ans += 1
+            left += 1
+
+        return ans

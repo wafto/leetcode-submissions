8d75f93a9e99c960777d72a7de74e7e8d50d5ee6
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-02-06 23:34:07 +0000

    Code challenge submission for question: 167 Two Sum II - Input Array Is Sorted

--- 167 Two Sum II - Input Array Is Sorted/167two-sum-ii-input-array-is-sorted.py
+++ 167 Two Sum II - Input Array Is Sorted/167two-sum-ii-input-array-is-sorted.py
@@ -3,10 +3,10 @@ class Solution:
         left, right = 0, len(numbers) - 1
 
         while left < right:
-            if numbers[left] + numbers[right] == target:
+            total = numbers[left] + numbers[right]
+            if total == target:
                 return [left + 1, right + 1]
-            
-            if numbers[left] + numbers[right] < target:
+            if total < target:
                 left += 1
             else:
                 right -= 1

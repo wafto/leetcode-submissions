107eb4a61c5075049a999a0213019efc245f04d6
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-02-10 01:57:44 +0000

    Code challenge submission for question: 1091 Shortest Path in Binary Matrix

--- 1091 Shortest Path in Binary Matrix/1091shortest-path-in-binary-matrix.py
+++ 1091 Shortest Path in Binary Matrix/1091shortest-path-in-binary-matrix.py
@@ -1,38 +1,42 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        neighbors = [
-            (-1, -1), (-1,  0), (-1, 1),
-            ( 0, -1),           ( 0, 1),
-            ( 1, -1), ( 1,  0), ( 1, 1),
-        ]
-
         rows, cols = len(grid), len(grid[0])
+        visited = set()
         queue = deque()
+        path = 0
 
         if grid[0][0] == 0:
             queue.append((0, 0))
-            grid[0][0] = 2
-        else:
-            return -1
 
-        def queueNeighbors(r: int, c: int):
-            for rn, cn in neighbors:
-                r2 = r + rn
-                c2 = c + cn
-                if r2 < 0 or c2 < 0 or r2 >= rows or c2 >= cols or grid[r2][c2] != 0:
-                    continue
-                queue.append((r2, c2))
-                grid[r2][c2] = 2
-
-        path = 1
         while queue:
+            path += 1
             for _ in range(len(queue)):
                 r, c = queue.popleft()
+
                 if r == rows - 1 and c == cols - 1:
                     return path
-                queueNeighbors(r, c)
-            path += 1
+                
+                neighbors = [
+                    (r - 1, c - 1), 
+                    (r - 1, c), 
+                    (r - 1, c + 1), 
+                    (r, c - 1), 
+                    (r, c + 1), 
+                    (r + 1, c - 1), 
+                    (r + 1, c),
+                    (r + 1, c + 1),
+                ]
 
+                for nr, nc in neighbors:
+                    if nr < 0 or nc < 0 or nr >= rows or nc >= cols or (nr, nc) in visited:
+                        continue
+                    visited.add((nr, nc))
+                    if grid[nr][nc] == 0:
+                        queue.append((nr, nc))
+                
         return -1
 
+        
                 
+
+            

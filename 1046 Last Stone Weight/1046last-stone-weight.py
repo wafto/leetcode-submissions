fb9f36fed95e75378b02d4f3fea8ab65e30fab8a
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-02-13 20:15:20 +0000

    Code challenge submission for question: 1046 Last Stone Weight

--- 1046 Last Stone Weight/1046last-stone-weight.py
+++ 1046 Last Stone Weight/1046last-stone-weight.py
@@ -2,9 +2,9 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         stones = [-s for s in stones]
         heapq.heapify(stones)
+
         while len(stones) > 1:
-            s1 = heapq.heappop(stones)
-            s2 = heapq.heappop(stones)
-            res = abs(s1 - s2) * -1
-            heapq.heappush(stones, res)
-        return abs(stones[0]) if stones else 0
+            remain = abs(heapq.heappop(stones) - heapq.heappop(stones))
+            heapq.heappush(stones, -remain)
+
+        return -stones[0]

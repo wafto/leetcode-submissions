ca00bec9952d3eb36a0f517f88061d9021378549
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-02-15 03:19:55 +0000

    Code challenge submission for question: 2698 Find the Punishment Number of an Integer

---
+++ 2698 Find the Punishment Number of an Integer/2698find-the-punishment-number-of-an-integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        ans = 0
+
+        def partition(i: int, cur: int, target: int, string: str) -> bool:
+            if i == len(string) and target == cur:
+                return True
+
+            for j in range(i, len(string)):
+                s = string[i: j + 1]
+                if partition(j + 1, cur + int(s), target, string):
+                    return True
+            
+            return False
+        
+        for i in range(1, n + 1):
+            if partition(0, 0, i, str(i * i)):
+                ans += i * i
+
+        return ans
+
+

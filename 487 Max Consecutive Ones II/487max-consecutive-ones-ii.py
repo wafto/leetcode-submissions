cabcb8c058479d7a3605b2c7d124b56e2fb0bdc7
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-02-23 03:07:16 +0000

    Code challenge submission for question: 487 Max Consecutive Ones II

--- 487 Max Consecutive Ones II/487max-consecutive-ones-ii.py
+++ 487 Max Consecutive Ones II/487max-consecutive-ones-ii.py
@@ -1,16 +1,13 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        curr, prev, mx, zero = 0, 0, 0, 0
+        curr, prev, zero, maximal = 0, 0, 0, 0
 
-        if len(nums) == 1:
-            return 1
-
-        for i in range(len(nums)):
-            curr += nums[i]
-            if nums[i] == 0:
+        for num in nums:
+            curr += num
+            if num == 0:
+                zero = 1
                 prev = curr
                 curr = 0
-                zero = 1
-            mx = max(mx, prev + curr + zero)
+            maximal = max(maximal, prev + zero + curr)
 
-        return mx
+        return maximal

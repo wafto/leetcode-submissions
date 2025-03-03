c47e647272c556b59a38ce1603b7048d7320ab85
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-03-03 22:48:12 +0000

    Code challenge submission for question: 1143 Longest Common Subsequence

--- 1143 Longest Common Subsequence/1143longest-common-subsequence.py
+++ 1143 Longest Common Subsequence/1143longest-common-subsequence.py
@@ -1,13 +1,16 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        memo = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        n, m = len(text1), len(text2)
+        prev = [0] * (m + 1)
 
-        for i in range(len(text1) - 1, -1, -1):
-            for j in range(len(text2) - 1, -1, -1):
+        for i in range(n - 1, -1, -1):
+            curr = [0] * (m + 1)
+            for j in range(m - 1, -1, -1):
                 if text1[i] == text2[j]:
-                    memo[i][j] = 1 + memo[i + 1][j + 1]
+                    curr[j] = prev[j + 1] + 1
                 else:
-                    memo[i][j] = max(memo[i][j + 1], memo[i + 1][j])
+                    curr[j] = max(curr[j + 1], prev[j])
+            prev = curr
         
-        return memo[0][0]
+        return prev[0]
         

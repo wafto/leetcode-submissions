067afb808b33169dad8ae1576be64d9a88562943
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-03-04 17:44:37 +0000

    Code challenge submission for question: 188 Best Time to Buy and Sell Stock IV

---
+++ 188 Best Time to Buy and Sell Stock IV/188best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[[0] * (k + 1) for _ in range(2)] for __ in range(n + 1)]
+        
+        for i in range(n - 1, -1, -1):
+            for remain in range(1, k + 1):
+                for holding in range(2):
+
+                    ans = dp[i + 1][holding][remain]
+                    
+                    if holding:
+                        ans = max(ans, prices[i] + dp[i + 1][0][remain - 1])
+                    else:
+                        ans = max(ans, -prices[i] + dp[i + 1][1][remain])
+                
+                    dp[i][holding][remain] = ans
+                
+        return dp[0][0][k]
+

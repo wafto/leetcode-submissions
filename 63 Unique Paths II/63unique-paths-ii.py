f4be59b4f956bde9be9ff6e469b1599e17e3f88d
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-03-07 18:16:01 +0000

    Code challenge submission for question: 63 Unique Paths II

--- 63 Unique Paths II/63unique-paths-ii.py
+++ 63 Unique Paths II/63unique-paths-ii.py
@@ -2,24 +2,20 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         rows, cols = len(obstacleGrid), len(obstacleGrid[0])
 
-        # start or end are blocked
-        if obstacleGrid[0][0] == 1 or obstacleGrid[rows - 1][cols -1] == 1:
-            return 0
+        @cache
+        def dp(r: int, c: int) -> int:
+            if r == rows or c == cols:
+                return 0
 
-        prev = [0] * cols
-        for r in range(rows - 1, -1, -1):
-            curr = [0] * cols
-            if r == rows - 1:
-                curr[cols - 1] = 1
-            for c in range(cols - 1, -1, -1):
-                if obstacleGrid[r][c] != 1:
-                    curr[c] += prev[c]
-                    if c != cols - 1:
-                        curr[c] += curr[c + 1]
-            prev = curr
+            if obstacleGrid[r][c] == 1:
+                return 0
 
-        return prev[0]
+            if r == rows - 1 and c == cols - 1:
+                return 1
 
-        
+            return dp(r + 1, c) + dp(r, c + 1)
 
-        
+
+        return dp(0, 0)
+
+            

22e128909bdb720da2413b0f993ec4990433f7dd
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-03-13 21:35:34 +0000

    Code challenge submission for question: 344 Reverse String

--- 344 Reverse String/344reverse-string.py
+++ 344 Reverse String/344reverse-string.py
@@ -3,12 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        L, R = 0, len(s) - 1
-        
-        while L < R:
-            s[L], s[R] = s[R], s[L]
-            L += 1
-            R -= 1
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
             
+        
         
         

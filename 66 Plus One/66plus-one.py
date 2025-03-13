b2cc7f8cf09d45c9bd5dc32bfeed11c27d73e489
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-03-13 18:26:33 +0000

    Code challenge submission for question: 66 Plus One

--- 66 Plus One/66plus-one.py
+++ 66 Plus One/66plus-one.py
@@ -1,15 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        size = len(digits)
-        carry = 1
-        
-        for i in range(size):
-            index = size - 1 - i
-            total = digits[index] + carry
-            digits[index] = total % 10
-            carry = 1 if total >= 10 else 0
+        n, acc = len(digits), 1
+        ans = deque()
 
-        if carry == 1:
-            digits.insert(0, 1)
+        for i in range(n - 1, -1, -1):
+            addition = digits[i] + acc
+            ans.appendleft(addition % 10)
+            acc = 1 if addition == 10 else 0
 
-        return digits
+        if acc:
+            ans.appendleft(1)
+
+        return list(ans)

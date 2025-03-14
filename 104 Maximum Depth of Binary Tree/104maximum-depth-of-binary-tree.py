f2134fc5a4f3ae3cd5330950885f56244c91924f
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-03-14 19:04:19 +0000

    Code challenge submission for question: 104 Maximum Depth of Binary Tree

--- 104 Maximum Depth of Binary Tree/104maximum-depth-of-binary-tree.py
+++ 104 Maximum Depth of Binary Tree/104maximum-depth-of-binary-tree.py
@@ -9,15 +9,4 @@ class Solution:
         if not root:
             return 0
 
-        stack = [(root, 1)]
-        ans = 0
-
-        while stack:
-            node, depth = stack.pop()
-            ans = max(ans, depth)
-            if node.left:
-                stack.append((node.left, depth + 1))
-            if node.right:
-                stack.append((node.right, depth + 1))
-
-        return ans
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right)) 

b2c32c6749ba675038575e3f250ebc3926a1047d
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-03-17 06:00:25 +0000

    Code challenge submission for question: 88 Merge Sorted Array

--- 88 Merge Sorted Array/88merge-sorted-array.py
+++ 88 Merge Sorted Array/88merge-sorted-array.py
@@ -3,29 +3,32 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
         if n == 0:
             return
-        
-        i, j = 0, 0
-        
-        while i < m + n and j < n:
-            if nums1[i] > nums2[j]:
-                k = m + n - 1
-                while k > i:
-                    nums1[k] = nums1[k - 1]
-                    k -= 1
-                nums1[i] = nums2[j]
-                j += 1
-            i += 1
-                
-        k = m + j
-                
-        while k < m + n:
-            nums1[k] = nums2[j]
-            k += 1
-            j += 1
 
-            
-            
-        
-        
+        if m == 0:
+            for i in range(n):
+                nums1[i] = nums2[i]
+
+        while k >= 0 and i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        while i >= 0:
+            nums1[k] = nums1[i]
+            i, k = i - 1, k - 1
+
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j, k = j - 1, k - 1
+
+        print(i, j, k)

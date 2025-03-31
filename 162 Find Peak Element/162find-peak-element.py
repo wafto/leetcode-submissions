bb4a66ee3dce2598e93d80e51cc8d304a68fe78c
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-03-31 23:14:31 +0000

    Code challenge submission for question: 162 Find Peak Element

---
+++ 162 Find Peak Element/162find-peak-element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        left, right = 0, n - 1
+
+        while left < right:
+            mid = (right + left) // 2
+
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+
+        return right
+
+
+
+        

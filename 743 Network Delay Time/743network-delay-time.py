92a686b1e220c7d803ee8dc13d0ef25699c723a1
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-04-25 21:41:11 +0000

    Code challenge submission for question: 743 Network Delay Time

--- 743 Network Delay Time/743network-delay-time.py
+++ 743 Network Delay Time/743network-delay-time.py
@@ -1,27 +1,24 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adj = {i: [] for i in range(n + 1)}
+        graph = {i: [] for i in range(1, n + 1)}
 
         for u, v, w in times:
-            adj[u].append((v, w))
+            graph[u].append((v, w))
 
         minheap = [(0, k)]
-        shortest = defaultdict(int)
+        shortest = {}
 
         while minheap:
-            cost, node = heapq.heappop(minheap)
-            
+            weight, node = heapq.heappop(minheap)
+
             if node in shortest:
                 continue
-            
-            shortest[node] = cost
 
-            for v, w in adj[node]:
-                if v not in shortest:
-                    heapq.heappush(minheap, (cost + w, v))
+            shortest[node] = weight
 
-        if len(shortest) != n:
-            return -1
+            for nei_node, nei_weight in graph[node]:
+                if nei_node not in shortest:
+                    heapq.heappush(minheap, (weight + nei_weight, nei_node))
 
-        return max(shortest.values())
-        
+        return -1 if len(shortest) != n else max(shortest.values())
+

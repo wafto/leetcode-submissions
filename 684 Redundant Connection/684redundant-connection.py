461b438170986bbeb80a92f7d1ee783d1df213c5
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-04-28 21:40:54 +0000

    Code challenge submission for question: 684 Redundant Connection

--- 684 Redundant Connection/684redundant-connection.py
+++ 684 Redundant Connection/684redundant-connection.py
@@ -1,36 +1,43 @@
 class UnionFind:
     def __init__(self, n: int):
-        self.par = {}
+        self.parent = {}
         self.rank = {}
+
         for i in range(1, n + 1):
-            self.par[i] = i
+            self.parent[i] = i
             self.rank[i] = 0
 
     def find(self, n: int) -> int:
-        p = self.par[n]
-        while p != self.par[p]:
-            self.par[p] = self.par[self.par[p]]
-            p = self.par[p]
+        p = self.parent[n]
+        while p != self.parent[p]:
+            self.parent[p] = self.parent[self.parent[p]] # O(n) -> O(logn)
+            p = self.parent[p]
         return p
 
     def union(self, n1: int, n2: int) -> bool:
         p1, p2 = self.find(n1), self.find(n2)
+
         if p1 == p2:
-            return False
+            return False # We have a cycle
+
         if self.rank[p1] > self.rank[p2]:
-            self.par[p2] = p1
+            self.parent[p2] = self.parent[p1]
         elif self.rank[p2] > self.rank[p1]:
-            self.par[p1] = p2
+            self.parent[p1] = self.parent[p2]
         else:
-            self.par[p1] = p2
-            self.rank[p2] += 1
+            self.parent[p2] = self.parent[p1]
+            self.rank[p1] += 1
+        
         return True
-    
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        unionFind = UnionFind(len(edges))
+        nodes = len(edges)
+        uf = UnionFind(nodes)
 
         for n1, n2 in edges:
-            if not unionFind.union(n1, n2):
+            if not uf.union(n1, n2):
                 return [n1, n2]
+
+        return []
+    

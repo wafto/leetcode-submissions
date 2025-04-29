22ae4e379ffff6304a14b856871dff5b581a33fd
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-04-29 23:30:51 +0000

    Code challenge submission for question: 17 Letter Combinations of a Phone Number

--- 17 Letter Combinations of a Phone Number/17letter-combinations-of-a-phone-number.py
+++ 17 Letter Combinations of a Phone Number/17letter-combinations-of-a-phone-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        combinations = []
+        ans = []
+        n = len(digits)
         mapping = {
             '2': ['a', 'b', 'c'],
             '3': ['d', 'e', 'f'],
@@ -9,24 +10,21 @@ class Solution:
             '6': ['m', 'n', 'o'],
             '7': ['p', 'q', 'r', 's'],
             '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z'],
+            '9': ['w', 'x', 'y', 'z'], 
         }
 
-        def backtracking(curr: List[str], i: int) -> None:
-            if len(curr) == len(digits):
-                if curr:
-                    combinations.append(''.join(curr))
+        if not n:
+            return ans
+
+        def backtracking(i: int, curr: List[str]) -> None:
+            if len(curr) == n:
+                ans.append(''.join(curr))
                 return
 
-            for j in range(i, len(digits)):
-                for c in mapping[digits[j]]:
-                    curr.append(c)
-                    backtracking(curr, j + 1)
-                    curr.pop()
-
-        backtracking([], 0)
-        return combinations
-
-            
-
-            
+            for ch in mapping[digits[i]]:
+                curr.append(ch)
+                backtracking(i + 1, curr)
+                curr.pop()
+        
+        backtracking(0, [])
+        return ans

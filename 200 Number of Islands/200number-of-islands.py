9125f9f4090c411ff9a5b187dfac22da5de036be
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-04 01:08:14 +0000

    Code challenge submission for question: 200 Number of Islands

--- 200 Number of Islands/200number-of-islands.py
+++ 200 Number of Islands/200number-of-islands.py
@@ -2,23 +2,24 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
         islands = 0
+        seen = set()
 
-        def dfs(r: int, c: int) -> int:
+        def dfs(r: int, c: int) -> None:
             if r < 0 or c < 0 or r >= rows or c >= cols:
-                return 0 
-            if grid[r][c] == '0' or grid[r][c] == 'x':
-                return 0
-            grid[r][c] = 'x'
+                return
+            
+            if grid[r][c] != '1' or (r, c) in seen:
+                return
+            
+            seen.add((r, c))
+
             for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
                 dfs(nr, nc)
-            return 1
-             
+                        
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1':
-                    islands += dfs(r, c)
+                if grid[r][c] == '1' and (r, c) not in seen:
+                    islands += 1
+                    dfs(r, c)
 
         return islands
-            
-
-

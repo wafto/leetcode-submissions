56398e60fa6df4e8ab2800330b16a7bfd9b61589
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-13 01:18:42 +0000

    Code challenge submission for question: 1 Two Sum

--- 1 Two Sum/1two-sum.py
+++ 1 Two Sum/1two-sum.py
@@ -1,11 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {} # num -> index
+        hashmap = {} # num: index
 
         for i in range(len(nums)):
             diff = target - nums[i]
+
             if diff in hashmap:
                 return [hashmap[diff], i]
+
             hashmap[nums[i]] = i
 
-        return [] 
+        return []

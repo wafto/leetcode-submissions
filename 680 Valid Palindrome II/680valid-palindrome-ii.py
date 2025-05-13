045e449d905d10b5ee7137e9efca51e9a5c1862c
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-13 20:38:59 +0000

    Code challenge submission for question: 680 Valid Palindrome II

--- 680 Valid Palindrome II/680valid-palindrome-ii.py
+++ 680 Valid Palindrome II/680valid-palindrome-ii.py
@@ -1,21 +1,18 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         
-        def palindrome(s: str, left: int, right: int) -> bool:
+        def is_palindrome(left: int, right: int) -> bool:
             while left < right:
-                if s[left] == s[right]:
-                    left +=1
-                    right -=1
-                else:
+                if s[left] != s[right]:
                     return False
+                left, right = left + 1, right - 1
             return True
 
         left, right = 0, len(s) - 1
 
         while left < right:
-            if s[left] == s[right]:
-                left +=1
-                right -=1
-            else:
-                return palindrome(s, left + 1, right) or palindrome(s, left, right - 1)
+            if s[left] != s[right]:
+                return is_palindrome(left + 1, right) or is_palindrome(left, right - 1)
+            left, right = left + 1, right - 1
+
         return True

e9e4210fa031d201920c4a30064cad91e86b98ed
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-14 22:16:00 +0000

    Code challenge submission for question: 141 Linked List Cycle

--- 141 Linked List Cycle/141linked-list-cycle.py
+++ 141 Linked List Cycle/141linked-list-cycle.py
@@ -9,9 +9,9 @@ class Solution:
         slow, fast = head, head
 
         while fast and fast.next:
-            fast = fast.next.next
             slow = slow.next
-            if fast == slow:
+            fast = fast.next.next
+            if slow == fast:
                 return True
 
         return False

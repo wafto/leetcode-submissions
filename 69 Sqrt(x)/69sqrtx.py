d6ae8aadac2dc504876a48138fb17ae53da08697
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-14 17:11:46 +0000

    Code challenge submission for question: 69 Sqrt(x)

---
+++ 69 Sqrt(x)/69sqrtx.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        L, R = 0, (x // 2) + 1
+
+        while L <= R:
+            M = (R + L) // 2
+            C = M * M
+
+            if C == x:
+                return M
+
+            if C > x:
+                R = M - 1
+            else:
+                L = M + 1
+
+        return R

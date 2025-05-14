24e02aa4f81d95815ab21c3a382c4eb1c57308c8
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-14 23:58:02 +0000

    Code challenge submission for question: 100 Same Tree

--- 100 Same Tree/100same-tree.py
+++ 100 Same Tree/100same-tree.py
@@ -6,13 +6,17 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
+        
+        def same(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+            if not p and not q:
+                return True
 
-        if not p or not q:
-            return False
+            if not p or not q:
+                return False
 
-        if p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            if p.val != q.val:
+                return False
 
-        return False
+            return same(p.left, q.left) and same(p.right, q.right)
+
+        return same(p, q)

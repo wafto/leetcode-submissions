6c43bf6f2fef848fcf0f4cca5e39113bc1d9958f
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-16 21:01:50 +0000

    Code challenge submission for question: 191 Number of 1 Bits

--- 191 Number of 1 Bits/191number-of-1-bits.py
+++ 191 Number of 1 Bits/191number-of-1-bits.py
@@ -1,9 +1,11 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        ans = 0
         
-        while n > 0:
-            ans += n & 1
-            n >>= 1
-            
-        return ans
+        @cache
+        def hamming(n: int) -> int: 
+            if n <= 0:
+                return 0
+            return hamming(n >> 1) + (n & 1)
+
+        return hamming(n)
+

6cbc6918ab8753482d79da2030e749aecd61dfed
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-16 21:03:26 +0000

    Code challenge submission for question: 136 Single Number

--- 136 Single Number/136single-number.py
+++ 136 Single Number/136single-number.py
@@ -1,8 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        mask = 0
+        answer = nums[0]
 
-        for num in nums:
-            mask ^= num
+        for i in range(1, len(nums)):
+            answer ^= nums[i]
 
-        return mask
+        return answer
+        

2b9e53ba35b2a77cb5965dc65db272c5665985d6
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-16 20:43:39 +0000

    Code challenge submission for question: 338 Counting Bits

--- 338 Counting Bits/338counting-bits.py
+++ 338 Counting Bits/338counting-bits.py
@@ -1,11 +1,8 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        dp = [0] * (n + 1)
-        offset = 1
-        
-        for i in range(1, n + 1):
-            if offset * 2 == i:
-                offset = i
-            dp[i] = 1 + dp[i - offset]
+        weights = [0] * (n + 1)
 
-        return dp
+        for i in range(1, n + 1):
+            weights[i] = weights[i >> 1] + (i & 1)
+
+        return weights

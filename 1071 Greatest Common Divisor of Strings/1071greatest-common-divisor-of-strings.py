be7b879b6b4d7b750d20e31c405544dd56e951d8
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-17 00:28:40 +0000

    Code challenge submission for question: 1071 Greatest Common Divisor of Strings

--- 1071 Greatest Common Divisor of Strings/1071greatest-common-divisor-of-strings.py
+++ 1071 Greatest Common Divisor of Strings/1071greatest-common-divisor-of-strings.py
@@ -1,20 +1,19 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        n1, n2 = len(str1), len(str2)
-        smallest = str1 if n1 < n2 else str2
         
-        def valid(i: int) -> bool:
-            if n1 % i or n2 % i:
-                return False
-            sub = smallest[:i]
-            d1, d2 = n1 // i, n2 // i
+        def gcd(a: int, b: int) -> int:
+            while b:
+                a, b = b, a % b
+            return a
 
-            return str1 == sub * d1 and str2 == sub * d2
+        n, m = len(str1), len(str2)
+        g = gcd(n, m)
+        p = str1[: g]
 
-        for i in range(min(n1, n2), 0, -1):
-            if valid(i):
-                return smallest[:i]
-        
+        if p * (n // g) == str1 and p * (m // g) == str2:
+            return p
+
         return ''
+
 
         

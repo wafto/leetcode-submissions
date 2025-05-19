38d4a52a06dfea6d8f56828bd7e6f650ec95166b
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-19 04:56:20 +0000

    Code challenge submission for question: 994 Rotting Oranges

--- 994 Rotting Oranges/994rotting-oranges.py
+++ 994 Rotting Oranges/994rotting-oranges.py
@@ -2,31 +2,37 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         queue = deque()
-        neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        fresh = set()
+        seen = set()
+        oranges = 0
 
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 2:
-                    queue.append((i, j))
-                if grid[i][j] == 1:
-                    fresh.add((i, j))
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+                    seen.add((r,c))
+                    oranges += 1
+                elif grid[r][c] == 1:
+                    oranges += 1
 
-        if not fresh:
+        if not oranges:
             return 0
-                
+
         minutes = -1
+
         while queue:
             for _ in range(len(queue)):
                 r, c = queue.popleft()
-                for rn, cn in neighbors:
-                    rx = r + rn
-                    cx = c + cn
-                    if rx >= 0 and cx >= 0 and rx < rows and cx < cols and grid[rx][cx] == 1: 
-                        queue.append((rx, cx))
-                        grid[rx][cx] = 2
-                        fresh.remove((rx, cx))
+
+                for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                    if nr < 0 or nc < 0 or nr == rows or nc == cols:
+                        continue
+                    if (nr, nc) in seen or grid[nr][nc] != 1:
+                        continue
+                    
+                    grid[nr][nc] == 2
+                    seen.add((nr, nc))
+                    queue.append((nr, nc))
+    
             minutes += 1
-        return minutes if not fresh else -1
-                
-                
+
+        return minutes if oranges == len(seen) else -1

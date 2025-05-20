4ede1fe4d13fbb83eca253749f64cf19d046a989
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-20 01:38:33 +0000

    Code challenge submission for question: 304 Range Sum Query 2D - Immutable

--- 304 Range Sum Query 2D - Immutable/304range-sum-query-2d-immutable.py
+++ 304 Range Sum Query 2D - Immutable/304range-sum-query-2d-immutable.py
@@ -2,23 +2,22 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         rows, cols = len(matrix), len(matrix[0])
-        self.sumprefix = [[0] * (cols + 1) for _ in range(rows + 1)]
-        for r in range(rows):
-            prefix = 0
-            for c in range(cols):
-                prefix += matrix[r][c]
-                above = self.sumprefix[r][c + 1]
-                self.sumprefix[r + 1][c + 1] = prefix + above   
+        self.ps = [[0] * (cols + 1) for _ in range(rows + 1)]
 
+        for r in range(rows):
+            ps = 0
+            for c in range(cols):
+                ps += matrix[r][c]
+                self.ps[r + 1][c + 1] = ps + self.ps[r][c + 1]
+        
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         r1, c1, r2, c2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
-
-        br = self.sumprefix[r2][c2]
-        above = self.sumprefix[r1 - 1][c2]
-        left = self.sumprefix[r2][c1 -1]
-        tl = self.sumprefix[r1 - 1][c1 - 1]
-
-        return br - above - left + tl
+        br = self.ps[r2][c2]
+        tr = self.ps[r1 - 1][c2]
+        tl = self.ps[r1 - 1][c1 - 1]
+        bl = self.ps[r2][c1 - 1]
+        
+        return br - tr - bl + tl        
 
 
 # Your NumMatrix object will be instantiated and called as such:

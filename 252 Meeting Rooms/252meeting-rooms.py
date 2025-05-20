9ad119d2c090bfaab70ec7562fa355f1ffb7e027
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-05-20 02:41:03 +0000

    Code challenge submission for question: 252 Meeting Rooms

--- 252 Meeting Rooms/252meeting-rooms.py
+++ 252 Meeting Rooms/252meeting-rooms.py
@@ -1,9 +1,17 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort()
+        intervals = sorted([(r[0], r[1]) for r in intervals])
+
+        if not intervals:
+            return True
+
+        last = intervals[0][1]
 
         for i in range(1, len(intervals)):
-            if intervals[i - 1][1] > intervals[i][0]:
+            start, end = intervals[i]
+            if start < last:
                 return False
+            last = max(last, end)
 
         return True
+

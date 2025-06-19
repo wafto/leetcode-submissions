7fa94ea16fbf123b5192d95995fa9cdd3f702d4b
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-06-19 02:49:55 +0000

    Code challenge submission for question: 14 Longest Common Prefix

--- 14 Longest Common Prefix/14longest-common-prefix.py
+++ 14 Longest Common Prefix/14longest-common-prefix.py
@@ -1,30 +1,10 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-        self.prefix_index = float('inf')
-
-    def add(self, word: str) -> None:
-        curr = self.root
-        for i, c in enumerate(word):
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            if len(curr.children) > 1:
-                self.prefix_index = min(self.prefix_index, i)
-            curr = curr.children[c]
-        self.prefix_index = min(self.prefix_index, len(word))
-         
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        trie = Trie()
+        prefix = strs[0]
 
-        for word in strs:
-            trie.add(word)
+        for i in range(1, len(strs)):
+            word = strs[i]
+            while not word.startswith(prefix):
+                prefix = prefix[: len(prefix) - 1]
 
-        if not trie.prefix_index:
-            return ''
-        else:
-            return strs[0][:trie.prefix_index]
+        return prefix

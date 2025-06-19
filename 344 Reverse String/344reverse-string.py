b8e69cb5e6b241e32dbc51069ec6983d4c682c86
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-06-19 03:36:33 +0000

    Code challenge submission for question: 344 Reverse String

--- 344 Reverse String/344reverse-string.py
+++ 344 Reverse String/344reverse-string.py
@@ -3,7 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        left, right = 0, len(s) - 1
+        n = len(s)
+        left, right = 0, n - 1
+        
         while left < right:
             s[left], s[right] = s[right], s[left]
             left, right = left + 1, right - 1

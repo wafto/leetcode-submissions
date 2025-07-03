5cf6a9c72b7e3dbaf5792a6a96b609d560c3acc4
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-07-03 21:23:54 +0000

    Code challenge submission for question: 13 Roman to Integer

--- 13 Roman to Integer/13roman-to-integer.py
+++ 13 Roman to Integer/13roman-to-integer.py
@@ -1,32 +1,25 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        symbols = {
-            "M":  1000,
-            "CM": 900,
-            "D":  500,
-            "CD": 400,
-            "C":  100,
-            "XC": 90,
-            "L":  50,
-            "XL": 40,
-            "X":  10,
-            "IX": 9,
-            "V":  5,
-            "IV": 4,
-            "I":  1 
+        ans, n = 0, len(s)
+        hashmap = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000,
         }
 
-        output = 0
-        size = len(s)
         index = 0
-        
-        while index < size:
-            if s[index:index + 2] in symbols:
-                output += symbols[s[index:index + 2]]
+        while index < n:
+            if index + 1 < n and hashmap[s[index]] < hashmap[s[index + 1]]:
+                ans += hashmap[s[index + 1]] - hashmap[s[index]]
                 index += 2
             else:
-                output += symbols[s[index]]
+                ans += hashmap[s[index]]
                 index += 1
 
-        return output
+        return ans
+
 

2dea3665829d0bb1bd3905520e533b3a84269482
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-07-03 01:27:28 +0000

    Code challenge submission for question: 867 Transpose Matrix

---
+++ 867 Transpose Matrix/867transpose-matrix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(matrix), len(matrix[0])
+        ans = [[0] * rows for _ in range(cols)]
+
+        for r in range(rows):
+            for c in range(cols):
+                ans[c][r] = matrix[r][c]
+
+        return ans
+        

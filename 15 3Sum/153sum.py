6a08d2cda785c94903ed4c3ec171e02db9b81cc4
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-07-03 22:25:42 +0000

    Code challenge submission for question: 15 3Sum

--- 15 3Sum/153sum.py
+++ 15 3Sum/153sum.py
@@ -1,31 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans, n = [], len(nums)
         nums.sort()
-        n = len(nums)
-        ans = []
 
         for i in range(n - 2):
-            j, k = i + 1, n - 1
-
             if i > 0 and nums[i - 1] == nums[i]:
                 continue
-            
-            while j < k:
-                acc = nums[i] + nums[j] + nums[k]
 
-                if acc == 0:
+            j, k = i + 1, n - 1
+
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+
+                if total < 0:
+                    j += 1
+                elif total > 0:
+                    k -= 1
+                else:
                     ans.append([nums[i], nums[j], nums[k]])
                     j, k = j + 1, k - 1
 
-                    while j < k and nums[j - 1] == nums[j]:
-                        j += 1
+                    while j < n and nums[j - 1] == nums[j]:
+                        j+= 1
 
-                    while j < k and nums[k + 1] == nums[k]:
-                        k -= 1
-
-                elif acc > 0:
-                    k -= 1
-                else:
-                    j += 1
-
+                    while k + 1 < n - 1 and nums[k + 1] == nums[k]:
+                        k -= 1          
+    
         return ans

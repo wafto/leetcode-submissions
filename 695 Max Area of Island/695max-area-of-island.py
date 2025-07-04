4fa825ad9d7ef6faf23d845277c82000ca9fb22f
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-07-04 00:36:54 +0000

    Code challenge submission for question: 695 Max Area of Island

--- 695 Max Area of Island/695max-area-of-island.py
+++ 695 Max Area of Island/695max-area-of-island.py
@@ -1,24 +1,29 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-
-        def dfs(r: int, c: int) -> int:
-            if r < 0 or c < 0 or r == rows or c == cols:
-                return 0
+        maximal = 0
+        
+        def area(r: int, c: int) -> int:
             if grid[r][c] != 1:
                 return 0
-            size = 1
+
+            total = 1
             grid[r][c] = 2
+
             for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-                size += dfs(nr, nc)
-            return size
-        
-        max_island = 0
+                if nr < 0 or nc < 0 or nr >= rows or nc >= cols:
+                    continue
+                total += area(nr, nc)
+
+            return total
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    max_island = max(max_island, dfs(r, c))
-        
-        return max_island
+                    maximal = max(maximal, area(r, c))
 
+        return maximal
+
+
+        
+            

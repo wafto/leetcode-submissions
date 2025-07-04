05bec89030f4ac5646496569880c8f6fe462e5bc
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-07-04 02:36:40 +0000

    Code challenge submission for question: 216 Combination Sum III

--- 216 Combination Sum III/216combination-sum-iii.py
+++ 216 Combination Sum III/216combination-sum-iii.py
@@ -1,22 +1,26 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        combinations = []
+        nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        combs = []
 
-        def backtracking(curr: List[int], acc: int, start: int) -> None:
-            if acc == n and len(curr) == k:
-                combinations.append(curr.copy())
+        def backtracking(i: int, curr: List[int], total: int) -> None:
+            if i >= len(nums) and len(curr) == k and total == n:
+                combs.append(curr.copy())
+                return
+            
+            if i >= len(nums):
                 return
 
-            if acc > n or len(curr) > k:
-                return
-
-            for num in range(start, 10):
-                if acc + num > n:
-                    continue
-                curr.append(num)
-                backtracking(curr, acc + num, num + 1)
+            if total + nums[i] <= n:
+                curr.append(nums[i])
+                backtracking(i + 1, curr, total + nums[i])
                 curr.pop()
 
-        backtracking([], 0, 1)
-        return combinations
-        
+            backtracking(i + 1, curr, total)
+
+        backtracking(0, [], 0)
+        return combs
+
+
+
+            

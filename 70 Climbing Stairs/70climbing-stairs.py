06aee9b38ce46330204390747361ed9a853634e1
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-07-05 04:45:14 +0000

    Code challenge submission for question: 70 Climbing Stairs

--- 70 Climbing Stairs/70climbing-stairs.py
+++ 70 Climbing Stairs/70climbing-stairs.py
@@ -1,13 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 1:
-            return n
-
+        
         dp = [1, 2]
 
-        for i in range(2, n):
-            steps = dp[0] + dp[1]
+        if n == 1:
+            return dp[0]
+        
+        if n == 2:
+            return dp[1]
+
+        for i in range(3, n + 1):
+            temp = dp[0]
             dp[0] = dp[1]
-            dp[1] = steps
+            dp[1] = temp + dp[1]
 
         return dp[1]

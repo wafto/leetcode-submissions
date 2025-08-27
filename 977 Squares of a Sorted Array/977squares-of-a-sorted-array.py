b7711f4af7e96dfdb3c42683b060042dd8e721eb
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-08-27 18:37:58 +0000

    Code challenge submission for question: 977 Squares of a Sorted Array

--- 977 Squares of a Sorted Array/977squares-of-a-sorted-array.py
+++ 977 Squares of a Sorted Array/977squares-of-a-sorted-array.py
@@ -1,18 +1,17 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        l, r, i = 0, n - 1, n - 1
-        output = [0] * n
-        
-        while l <= r:
-            if abs(nums[l]) > abs(nums[r]):
-                output[i] = nums[l] ** 2
-                l += 1
+        ans = [0] * n
+
+        left, right, curr = 0, n - 1, n - 1
+
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                ans[curr] = nums[left] ** 2
+                left += 1
             else:
-                output[i] = nums[r] ** 2
-                r -= 1
-            i -= 1
-            
-        return output
-                
-        
+                ans[curr] = nums[right] ** 2
+                right -= 1
+            curr -= 1
+
+        return ans

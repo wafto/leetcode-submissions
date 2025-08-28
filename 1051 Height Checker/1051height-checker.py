595c57b126fb5e1b001398e718ad68bbb721cddc
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-08-28 16:30:07 +0000

    Code challenge submission for question: 1051 Height Checker

--- 1051 Height Checker/1051height-checker.py
+++ 1051 Height Checker/1051height-checker.py
@@ -1,10 +1,10 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        expected = sorted(heights)
-        output = 0
-        
+        ordered = sorted(heights.copy())
+        ans = 0
+
         for i in range(len(heights)):
-            if heights[i] != expected[i]:
-                output += 1
-                
-        return output
+            if heights[i] != ordered[i]:
+                ans += 1
+
+        return ans

e7adc6b385e056ba4713c158a0a6d90e82ececdc
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-08-28 01:54:29 +0000

    Code challenge submission for question: 2558 Take Gifts From the Richest Pile

--- 2558 Take Gifts From the Richest Pile/2558take-gifts-from-the-richest-pile.py
+++ 2558 Take Gifts From the Richest Pile/2558take-gifts-from-the-richest-pile.py
@@ -1,15 +1,14 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        piles = [-n for n in gifts]
-        heapq.heapify(piles)
-        s = 0
-        while s < k:
-            choose = heapq.heappop(piles)
-            behind = math.floor(math.sqrt(choose * -1))
-            heapq.heappush(piles, behind * -1)
-            s += 1
-        remaining = 0
-        for n in piles:
-            remaining += n
-        return abs(remaining)
-         
+        heap = [-gift for gift in gifts]
+        heapq.heapify(heap)
+
+        while k > 0:
+            gift = -heapq.heappop(heap)
+            reduced = -int(math.sqrt(gift))
+            heapq.heappush(heap, reduced)
+            k -= 1
+
+        return -sum(heap)
+        
+            

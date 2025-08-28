43b21944341e9cb313c89625103c0b502b3fb15a
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-08-28 16:25:54 +0000

    Code challenge submission for question: 905 Sort Array By Parity

--- 905 Sort Array By Parity/905sort-array-by-parity.py
+++ 905 Sort Array By Parity/905sort-array-by-parity.py
@@ -1,14 +1,14 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        left, right = 0, len(nums) - 1
-        
-        while left < right:
-            while left < right and nums[left] & 1 == 0:
-                left += 1
-            while left < right and nums[right] & 1 == 1:
+        ans = [0] * len(nums)
+        left, right = 0, len(ans) - 1
+
+        for num in nums:
+            if num & 1:
+                ans[right] = num
                 right -= 1
-            nums[left], nums[right] = nums[right], nums[left]
-        
-        return nums
-            
-            
+            else:
+                ans[left] = num
+                left += 1
+
+        return ans

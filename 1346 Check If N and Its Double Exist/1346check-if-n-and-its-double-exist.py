1404ae4eb2562b2707822f97736e0f69775f1b2a
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-08-28 01:26:43 +0000

    Code challenge submission for question: 1346 Check If N and Its Double Exist

--- 1346 Check If N and Its Double Exist/1346check-if-n-and-its-double-exist.py
+++ 1346 Check If N and Its Double Exist/1346check-if-n-and-its-double-exist.py
@@ -1,10 +1,13 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        prevs = set()
-        
-        for j in range(len(arr)):
-            if arr[j] << 1 in prevs or (arr[j] & 1 == 0 and arr[j] >> 1 in prevs):
+        hashset = set()
+
+        for num in arr:
+            if num * 2 in hashset:
                 return True
-            prevs.add(arr[j])
-                
+            if num & 1 == 0 and num // 2 in hashset:
+                return True
+            hashset.add(num)
+
         return False
+

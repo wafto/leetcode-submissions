bd38a5bb55da4fdb725e3d3da1aab83f842957d2
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-08-31 19:41:53 +0000

    Code challenge submission for question: 118 Pascal's Triangle

--- 118 Pascal's Triangle/118pascals-triangle.py
+++ 118 Pascal's Triangle/118pascals-triangle.py
@@ -1,16 +1,12 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        tri = [[1]]
+        ans = [[1]]
 
-        for i in range(1, numRows):
-            size = i + 1
-            level = [1] * size
+        for num in range(1, numRows):
+            line = [1] * (num + 1)
+            prev = ans[-1]
+            for i in range(num - 1):
+                line[i + 1] = prev[i] + prev[i + 1]
+            ans.append(line)
 
-            if size > 2:
-                for j in range(1, size - 1):
-                    level[j] = tri[-1][j - 1] + tri[-1][j] 
-
-            tri.append(level)
-
-        return tri
-
+        return ans

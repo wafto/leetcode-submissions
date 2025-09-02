a11d5387042a6f7dc38a2f75c2a28aa4e513800d
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-02 22:13:09 +0000

    Code challenge submission for question: 151 Reverse Words in a String

--- 151 Reverse Words in a String/151reverse-words-in-a-string.py
+++ 151 Reverse Words in a String/151reverse-words-in-a-string.py
@@ -1,30 +1,19 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        
-        def split(sentence: str) -> List[str]:
-            left, n = 0, len(sentence)
-            words, word = [], deque()
+        buffer, ans = [], []
 
-            for right in range(n):
-                if sentence[right] != ' ':
-                    word.append(sentence[right])
-                    continue
-                
-                if word:
-                    words.append(''.join(word))
-                    word.clear()
-            if word:
-                words.append(''.join(word))
+        for c in s:
+            if c != ' ':
+                buffer.append(c)
+            elif buffer:
+                ans.append(''.join(buffer))
+                buffer = []
+            
+        if buffer:
+            ans.append(''.join(buffer))
 
-            return words
+        ans.reverse()
 
-        def reverse(words: List[str]) -> None:
-            left, right = 0, len(words) - 1
-            while left < right:
-                words[left], words[right] = words[right], words[left]
-                left, right = left + 1, right - 1
-    
-        words = split(s)
-        reverse(words)
+        return ' '.join(ans)
 
-        return ' '.join(words)
+

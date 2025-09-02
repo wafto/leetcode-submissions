b213936956bf2a362f55c58db2fca74059c076a1
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-02 21:01:53 +0000

    Code challenge submission for question: 344 Reverse String

--- 344 Reverse String/344reverse-string.py
+++ 344 Reverse String/344reverse-string.py
@@ -1,15 +1,10 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        n = len(s)
-        left, right = 0, n - 1
-        
+        left, right = 0, len(s) - 1
+
         while left < right:
             s[left], s[right] = s[right], s[left]
             left, right = left + 1, right - 1
-            
         
         
         

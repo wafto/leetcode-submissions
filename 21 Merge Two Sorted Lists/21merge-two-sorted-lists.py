446ab44bd6a9426d782dbda6e7f3e869faab4ae0
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-03 01:01:02 +0000

    Code challenge submission for question: 21 Merge Two Sorted Lists

--- 21 Merge Two Sorted Lists/21merge-two-sorted-lists.py
+++ 21 Merge Two Sorted Lists/21merge-two-sorted-lists.py
@@ -5,24 +5,22 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        sentinel = ListNode(0)
-        p, p1, p2 = sentinel, list1, list2
+        nhead = ListNode(-1)
+        p, p1, p2 = nhead, list1, list2
 
         while p1 and p2:
-            if p1.val <= p2.val:
-                sentinel.next = p1
+            if p1.val < p2.val:
+                p.next = p1
                 p1 = p1.next
             else:
-                sentinel.next = p2
+                p.next = p2 
                 p2 = p2.next
-            sentinel = sentinel.next
+            p = p.next
 
         if p1:
-            sentinel.next = p1
+            p.next = p1
 
         if p2:
-            sentinel.next = p2
+            p.next = p2
 
-        return p.next
-
-
+        return nhead.next

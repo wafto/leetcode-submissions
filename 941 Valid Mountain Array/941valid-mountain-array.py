403f88041b9fc5c72506c666d9c0c615f296e845
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-03 17:51:02 +0000

    Code challenge submission for question: 941 Valid Mountain Array

--- 941 Valid Mountain Array/941valid-mountain-array.py
+++ 941 Valid Mountain Array/941valid-mountain-array.py
@@ -1,20 +1,17 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        i, n = 1, len(arr)
+        n, i = len(arr), 0
 
         if n < 3:
             return False
 
-        while i < n and arr[i - 1] < arr[i]:
+        while i < n - 1 and arr[i] < arr[i + 1]:
             i += 1
 
-        if i == 0 or i == n or arr[i - 1] == arr[i]:
+        if i == 0 or i == n - 1 or arr[i] == arr[i + 1]:
             return False
 
-        while i < n and arr[i - 1] > arr[i]:
+        while i < n - 1 and arr[i] > arr[i + 1]:
             i += 1
 
-        if i != n:
-            return False
-
-        return True
+        return i == n - 1

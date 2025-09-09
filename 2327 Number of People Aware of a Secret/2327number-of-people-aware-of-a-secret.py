b3341b7e8e348c93942a938debb9bec41eac9e8f
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-09 16:19:42 +0000

    Code challenge submission for question: 2327 Number of People Aware of a Secret

---
+++ 2327 Number of People Aware of a Secret/2327number-of-people-aware-of-a-secret.py
@@ -0,0 +1,25 @@
+class Solution:
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        MOD = 10 ** 9 + 7
+
+        if n == 1:
+            return 1
+
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        window = 0
+
+        for i in range(2, n + 1):
+            enter = i - delay
+            leave = i - forget
+            
+            if enter >= 1:
+                window = window + dp[enter] % MOD
+
+            if leave >= 1:
+                window = (window - dp[leave] + MOD) % MOD
+
+            dp[i] = window
+            
+        start = max(1, n - forget + 1)
+        return sum(dp[start: n + 1]) % MOD

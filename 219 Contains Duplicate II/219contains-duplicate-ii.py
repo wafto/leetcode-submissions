68a2aa6487a64f656440d034903590fd16261ac7
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-10 21:36:13 +0000

    Code challenge submission for question: 219 Contains Duplicate II

--- 219 Contains Duplicate II/219contains-duplicate-ii.py
+++ 219 Contains Duplicate II/219contains-duplicate-ii.py
@@ -1,10 +1,10 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        hashmap = {} # num: index
+        hashmap = {}
 
         for i, num in enumerate(nums):
             if num in hashmap and i - hashmap[num] <= k:
                 return True
             hashmap[num] = i
 
-        return False 
+        return False

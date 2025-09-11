96bb981eee81c5d5e9ddf2876e79bea15f56c3c5
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-11 03:18:28 +0000

    Code challenge submission for question: 200 Number of Islands

--- 200 Number of Islands/200number-of-islands.py
+++ 200 Number of Islands/200number-of-islands.py
@@ -3,25 +3,21 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         islands = 0
 
-        def bfs(r: int, c: int) -> None:
-            queue = deque([(r, c)])
+        def dfs(r: int, c: int) -> None:
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] != '1':
+                return
 
-            while queue:
-                r, c = queue.popleft()
-                for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-                    if nr < 0 or nc < 0 or nr >= rows or nc >= cols:
-                        continue
-                    if grid[nr][nc] != '1':
-                        continue
-                    grid[nr][nc] = '0'
-                    queue.append((nr, nc))
+            grid[r][c] = '2'
+            
+            for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                dfs(nr, nc)
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
+                    dfs(r, c)
                     islands += 1
-                    bfs(r, c)
 
         return islands
+
         
-                    

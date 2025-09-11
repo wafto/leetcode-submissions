75dfadb72ce2d5bed33ee649cfff19bf9cb19473
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-11 18:31:33 +0000

    Code challenge submission for question: 771 Jewels and Stones

--- 771 Jewels and Stones/771jewels-and-stones.py
+++ 771 Jewels and Stones/771jewels-and-stones.py
@@ -1,9 +1,10 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         jewels = set(jewels)
-        ans = 0
-        
-        for stone in stones:
-            ans += 1 if stone in jewels else 0
+        count = 0
 
-        return ans
+        for stone in stones:
+            if stone in jewels:
+                count += 1
+
+        return count

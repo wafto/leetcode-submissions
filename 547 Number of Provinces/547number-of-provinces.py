75a022ad67b14fdb3452da5b5a27393e13b4af2a
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-11 03:05:59 +0000

    Code challenge submission for question: 547 Number of Provinces

--- 547 Number of Provinces/547number-of-provinces.py
+++ 547 Number of Provinces/547number-of-provinces.py
@@ -1,14 +1,14 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        graph = defaultdict(list)
+        graph = defaultdict(set)
         seen = set()
 
         for i in range(n):
             for j in range(n):
                 if isConnected[i][j]:
-                    graph[i].append(j)
-                    graph[j].append(i)
+                    graph[i].add(j)
+                    graph[j].add(i)
 
         def dfs(node: int) -> None:
             for nei in graph[node]:
@@ -16,11 +16,22 @@ class Solution:
                     seen.add(nei)
                     dfs(nei)
 
+        def bfs(node: int) -> None:
+            queue = deque([node])
+
+            while queue:
+                curr = queue.popleft()
+                for nei in graph[curr]:
+                    if nei not in seen:
+                        seen.add(nei)
+                        queue.append(nei)
+
         ans = 0
         for node in range(n):
             if node not in seen:
                 ans += 1
                 seen.add(node)
-                dfs(node)
+                bfs(node)
 
         return ans
+        

ef82ee7e463d5e720f1b7c1fff128be0fd4c1a7e
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-12 00:34:07 +0000

    Code challenge submission for question: 1219 Path with Maximum Gold

--- 1219 Path with Maximum Gold/1219path-with-maximum-gold.py
+++ 1219 Path with Maximum Gold/1219path-with-maximum-gold.py
@@ -6,33 +6,22 @@ class Solution:
             if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == 0:
                 return 0
 
-            gold, tmp = 0, grid[r][c]
+            gold = grid[r][c]
             grid[r][c] = 0
+            best = 0
+            
+            for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                best = max(best, backtracking(nr, nc))
 
-            for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
-                gold = max(gold, backtracking(nr, nc))
+            grid[r][c] = gold
 
-            grid[r][c] = tmp
-
-            return gold + grid[r][c]
+            return gold + best
 
         answer = 0
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] != 0:
+                if grid[r][c] > 0:
                     answer = max(answer, backtracking(r, c))
 
         return answer
-
-                
-                
-
-
-
-
-
-
-        
-
-        

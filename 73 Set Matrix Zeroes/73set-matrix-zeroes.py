ac90bc7c1a9ebba36361ee46b158be23abc12094
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-12 00:20:18 +0000

    Code challenge submission for question: 73 Set Matrix Zeroes

--- 73 Set Matrix Zeroes/73set-matrix-zeroes.py
+++ 73 Set Matrix Zeroes/73set-matrix-zeroes.py
@@ -1,20 +1,25 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         rows, cols = len(matrix), len(matrix[0])
-        rmap, cmap = set(), set()
 
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(1, rows):
+            for c in range(1, cols):
                 if matrix[r][c] == 0:
-                    rmap.add(r)
-                    cmap.add(c)
+                    matrix[0][c] = 0
+                    matrix[r][0] = 0
 
-        for r in rmap:
-            for c in range(cols):
-                matrix[r][c] = 0
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
 
-        for c in cmap:
+        if matrix[0][0] == 0:
             for r in range(rows):
-                matrix[r][c] = 0
+                matrix[r][0] = 0
+
+            for c in range(cols):
+                matrix[0][c] = 0 
+
+        
         
         

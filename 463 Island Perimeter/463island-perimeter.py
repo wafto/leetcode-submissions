c5b4f109a548b67c98116003692a71edf63ae1af
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-18 01:59:49 +0000

    Code challenge submission for question: 463 Island Perimeter

--- 463 Island Perimeter/463island-perimeter.py
+++ 463 Island Perimeter/463island-perimeter.py
@@ -5,9 +5,12 @@ class Solution:
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1:
-                    for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-                        if nr < 0 or nc < 0 or nr >= rows or nc >= cols or grid[nr][nc] == 0:
-                            perimeter += 1
+                if grid[r][c] != 1:
+                    continue
+
+                for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                    if nr < 0 or nc < 0 or nr >= rows or nc >= cols or grid[nr][nc] == 0:
+                        perimeter += 1
 
         return perimeter
+        

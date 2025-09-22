48c9a2e374be4f4d299f9f12300900538b743794
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-22 20:28:53 +0000

    Code challenge submission for question: 155 Min Stack

--- 155 Min Stack/155min-stack.py
+++ 155 Min Stack/155min-stack.py
@@ -2,16 +2,16 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-
+        
     def push(self, val: int) -> None:
         if self.stack:
-            self.stack.append((val, min(self.stack[-1][1], val)))
+            self.stack.append((val, min(val, self.stack[-1][1])))
         else:
             self.stack.append((val, val))
 
     def pop(self) -> None:
         self.stack.pop()
-        
+
     def top(self) -> int:
         return self.stack[-1][0]
 

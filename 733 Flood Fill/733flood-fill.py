e4b5105dde94eb197050330f366742b8f7b3006e
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-23 20:55:56 +0000

    Code challenge submission for question: 733 Flood Fill

--- 733 Flood Fill/733flood-fill.py
+++ 733 Flood Fill/733flood-fill.py
@@ -1,18 +1,21 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         rows, cols = len(image), len(image[0])
-        start = image[sr][sc]
+
+        starting_color = image[sr][sc]
 
         def dfs(r: int, c: int) -> None:
+            if r < 0 or c < 0 or r >= rows or c >= cols: 
+                return
+
+            if image[r][c] != starting_color or image[r][c] == color:
+                return
+            
             image[r][c] = color
 
             for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-                if nr < 0 or nc < 0 or nr >= rows or nc >= cols or image[nr][nc] != start:
-                    continue
-                if image[nr][nc] == color:
-                    continue
                 dfs(nr, nc)
 
         dfs(sr, sc)
-
-        return image
+        
+        return image 

886dad1612cd277099964c408f1c0757e840645b
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-23 22:18:10 +0000

    Code challenge submission for question: 542 01 Matrix

--- 542 01 Matrix/54201-matrix.py
+++ 542 01 Matrix/54201-matrix.py
@@ -1,34 +1,26 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         rows, cols = len(mat), len(mat[0])
-        output = [[0 for _ in range(cols)] for _ in range(rows)]
+        output = [[0] * cols for _ in range(rows)]
+        seen = set()
         queue = deque()
-        visited = set()
         distance = 0
 
         for r in range(rows):
             for c in range(cols):
                 if mat[r][c] == 0:
+                    seen.add((r, c))
                     queue.append((r, c))
-                    output[r][c] = 0
-                    visited.add((r, c))
 
         while queue:
             distance += 1
             for _ in range(len(queue)):
                 r, c = queue.popleft()
                 for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-                    if nr < 0 or nc < 0 or nr >= rows or nc >= cols or (nr, nc) in visited:
+                    if nr < 0 or nc < 0 or nr >= rows or nc >= cols or (nr, nc) in seen:
                         continue
-                    output[nr][nc] = distance
+                    seen.add((nr, nc))
                     queue.append((nr, nc))
-                    visited.add((nr, nc))
+                    output[nr][nc] = distance
 
         return output
-
-
-
-
-
-
-

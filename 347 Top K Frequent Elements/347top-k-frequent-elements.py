15a427a92d39f648baa12b17feb7fa36fee21fc3
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-23 23:33:23 +0000

    Code challenge submission for question: 347 Top K Frequent Elements

--- 347 Top K Frequent Elements/347top-k-frequent-elements.py
+++ 347 Top K Frequent Elements/347top-k-frequent-elements.py
@@ -1,13 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        data = [(-v, x) for x, v in count.items()]
-        heapify(data)
-        ans = []
+        count = {}
 
+        for num in nums:
+            if num not in count:
+                count[num] = 0
+            count[num] += 1
+
+        heap = [(-cnt, num) for num, cnt in count.items()]
+        heapify(heap)
+
+        ans = []
+        
         for _ in range(k):
-            ans.append(heappop(data)[1])
+            ans.append(heappop(heap)[1])
 
         return ans
-
-        

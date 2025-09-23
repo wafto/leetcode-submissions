40be0be6c0e234ee5ba7804b08fbe5e711fe6f8f
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-23 23:39:10 +0000

    Code challenge submission for question: 703 Kth Largest Element in a Stream

--- 703 Kth Largest Element in a Stream/703kth-largest-element-in-a-stream.py
+++ 703 Kth Largest Element in a Stream/703kth-largest-element-in-a-stream.py
@@ -1,22 +1,17 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.k = k
-        self.heap = nums
-        
-        heapq.heapify(self.heap)
-        
-        while len(self.heap) > self.k:
-            heapq.heappop(self.heap)
+        self.heap = [num for num in nums]
+        heapify(self.heap)
+        self.cap = k
+        while len(self.heap) > self.cap:
+            heappop(self.heap)
         
     def add(self, val: int) -> int:
-        heapq.heappush(self.heap, val)
-
-        while len(self.heap) > self.k:
-            heapq.heappop(self.heap)
-
+        heappush(self.heap, val)
+        while len(self.heap) > self.cap:
+            heappop(self.heap)
         return self.heap[0]
-        
 
 
 # Your KthLargest object will be instantiated and called as such:

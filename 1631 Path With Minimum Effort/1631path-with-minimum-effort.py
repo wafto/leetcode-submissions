7987bf536a0b9f24f1807b06c6bb9102a83c262f
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-23 03:17:09 +0000

    Code challenge submission for question: 1631 Path With Minimum Effort

--- 1631 Path With Minimum Effort/1631path-with-minimum-effort.py
+++ 1631 Path With Minimum Effort/1631path-with-minimum-effort.py
@@ -2,36 +2,29 @@ class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         rows, cols = len(heights), len(heights[0])
 
-        def effort_bfs(effort: int) -> bool:
-            queue = deque([(0, 0)])
-            seen = {(0, 0)}
+        heap = [(0, 0, 0)] # effort, row, col
+        efforts = {}
 
-            while queue:
-                r, c = queue.popleft()
-                if r == rows - 1 and c == cols - 1:
-                    return True
-                for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-                    if nr < 0 or nc < 0 or nr == rows or nc == cols or (nr, nc) in seen:
-                        continue
-                    diff = abs(heights[r][c] - heights[nr][nc])
-                    if diff > effort:
-                        continue
-                    seen.add((nr, nc))
-                    queue.append((nr, nc))
-            return False
+        while heap:
+            e, r, c, = heappop(heap)
 
-        left, right = 0, 0
+            if (r, c) in efforts:
+                continue
 
-        for r in range(rows):
-            for c in range(cols):
-                right = max(right, heights[r][c])
+            efforts[(r, c)] = e
 
-        while left <= right:
-            mid = (right + left) // 2
-            if effort_bfs(mid):
-                right = mid - 1
-            else:
-                left = mid + 1
+            if (rows - 1, cols - 1) in efforts:
+                return efforts[(rows - 1, cols - 1)]
 
-        return left
-                
+            for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                if nr < 0 or nc < 0 or nr >= rows or nc >= cols or (nr, nc) in efforts:
+                    continue
+
+                ne = max(e, abs(heights[r][c] - heights[nr][nc]))
+
+                heappush(heap, (ne, nr, nc))
+
+        #return efforts[(rows - 1, cols - 1)]
+
+
+            

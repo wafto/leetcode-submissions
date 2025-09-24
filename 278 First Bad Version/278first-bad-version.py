1385756e61f3e91f11d7e29acfe3c3608c2af59a
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-24 22:55:24 +0000

    Code challenge submission for question: 278 First Bad Version

--- 278 First Bad Version/278first-bad-version.py
+++ 278 First Bad Version/278first-bad-version.py
@@ -5,11 +5,11 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         left, right = 1, n
 
-        while left <= right:
+        while left < right:
             mid = (right + left) // 2
-
+            
             if isBadVersion(mid):
-                right = mid - 1
+                right = mid
             else:
                 left = mid + 1
 

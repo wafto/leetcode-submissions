5a97ecb1625565acafbcd27b3b5220c0f1734894
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-26 03:03:39 +0000

    Code challenge submission for question: 127 Word Ladder

--- 127 Word Ladder/127word-ladder.py
+++ 127 Word Ladder/127word-ladder.py
@@ -1,34 +1,35 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        idx_chars = defaultdict(set)
-        wordList = set(wordList)
+        if endWord not in wordList:
+            return 0
+
+        adj = defaultdict(list)
 
         for word in wordList:
-            for i, c in enumerate(word):
-                idx_chars[i].add(c)
-
-        def posibilities(word: str) -> List[str]:
-            pos = []
             for i in range(len(word)):
-                for c in idx_chars[i]:
-                    if word[i] == c:
-                        continue
-                    w = word[: i] + c + word[i + 1:]
-                    if w in wordList:
-                        pos.append(w)
-            return pos
+                pattern = word[:i] + '*' + word[i + 1:]
+                adj[pattern].append(word)
 
         queue = deque([(beginWord, 1)])
-        seen = {beginWord}
+        visited = set([beginWord])
 
         while queue:
-            node, steps = queue.popleft()
-            if node == endWord:
-                return steps
-            for nei in posibilities(node):
-                if nei in seen:
-                    continue
-                queue.append((nei, steps + 1))
-                seen.add(nei)
+            word, steps = queue.popleft()
 
-        return 0
+            if word == endWord:
+                return steps
+
+            for i in range(len(word)):
+                pattern = word[:i] + '*' + word[i + 1:]
+
+                for nei in adj[pattern]:
+                    if nei in visited:
+                        continue
+
+                    visited.add(nei)
+                    queue.append((nei, steps + 1))
+
+        return 0 
+            
+
+

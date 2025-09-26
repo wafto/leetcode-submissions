c9f27b61afd1923c0879dfc67db2bb310d58c41d
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-26 17:26:32 +0000

    Code challenge submission for question: 658 Find K Closest Elements

--- 658 Find K Closest Elements/658find-k-closest-elements.py
+++ 658 Find K Closest Elements/658find-k-closest-elements.py
@@ -1,11 +1,22 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        heap = []
-
-        for num in arr:
-            distance = abs(num - x)
-            heapq.heappush(heap, (-distance, -num))
-            if len(heap) > k:
-                heapq.heappop(heap)
-
-        return sorted([-p[1] for p in heap])
+        low = 0
+        high = len(arr) - k
+        
+        while low < high:
+            mid = low + (high-low)//2
+            
+            if x <= arr[mid]:
+                high = mid    
+            elif arr[mid + k] <= x:
+                low = mid + 1
+            else:
+                middist = abs(x-arr[mid])
+                midkdist = abs(x-arr[mid + k])
+                
+                if middist <= midkdist:
+                    high = mid
+                else:
+                    low = mid+1
+            
+        return arr[low: low+k]

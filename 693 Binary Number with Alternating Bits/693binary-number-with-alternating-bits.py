ffb33f590c077e7ad06acabea0e1f8e2e6f3fc18
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-28 22:07:17 +0000

    Code challenge submission for question: 693 Binary Number with Alternating Bits

---
+++ 693 Binary Number with Alternating Bits/693binary-number-with-alternating-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        while n > 0:
+            if n & 1 == (n >> 1) & 1:
+                return False
+            n >>= 1
+        
+        return True
+
+

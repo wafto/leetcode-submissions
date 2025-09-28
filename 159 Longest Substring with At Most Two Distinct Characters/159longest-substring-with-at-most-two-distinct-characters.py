70e56585c10eec7710f2211eab325fb98409dba1
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-28 23:02:36 +0000

    Code challenge submission for question: 159 Longest Substring with At Most Two Distinct Characters

--- 159 Longest Substring with At Most Two Distinct Characters/159longest-substring-with-at-most-two-distinct-characters.py
+++ 159 Longest Substring with At Most Two Distinct Characters/159longest-substring-with-at-most-two-distinct-characters.py
@@ -1,17 +1,23 @@
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
-        hashmap = defaultdict(int)
+        hashmap = {}
         left, longest = 0, 0
 
         for right in range(len(s)):
+            if s[right] not in hashmap:
+                hashmap[s[right]] = 0
             hashmap[s[right]] += 1
 
             while len(hashmap) > 2:
                 hashmap[s[left]] -= 1
+
                 if hashmap[s[left]] == 0:
                     del hashmap[s[left]]
-                left += 1
 
+                left += 1
+    
             longest = max(longest, right - left + 1)
 
         return longest
+
+                

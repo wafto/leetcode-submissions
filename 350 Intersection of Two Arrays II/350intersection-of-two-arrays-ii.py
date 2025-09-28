95ccc28d76fe00bf45135f783ce979cb8b485a2e
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-09-28 01:47:39 +0000

    Code challenge submission for question: 350 Intersection of Two Arrays II

--- 350 Intersection of Two Arrays II/350intersection-of-two-arrays-ii.py
+++ 350 Intersection of Two Arrays II/350intersection-of-two-arrays-ii.py
@@ -1,10 +1,19 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        count1, count2 = Counter(nums1), Counter(nums2)
-        answer = []
+        nums1.sort()
+        nums2.sort()
 
-        for num, cnt in count1.items():
-            if num in count2:
-                answer.extend([num] * min(cnt, count2[num]))
+        i, j, n, m = 0, 0, len(nums1), len(nums2)
+        ans = []
 
-        return answer
+        while i < n and j < m:
+            if nums1[i] == nums2[j]:
+                ans.append(nums1[i])
+                i, j = i + 1, j + 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                i += 1
+
+        return ans
+        

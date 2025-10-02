511fa5d33765a3fe47bf287cfded28fca46f203b
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-10-02 02:12:17 +0000

    Code challenge submission for question: 1137 N-th Tribonacci Number

--- 1137 N-th Tribonacci Number/1137n-th-tribonacci-number.py
+++ 1137 N-th Tribonacci Number/1137n-th-tribonacci-number.py
@@ -1,9 +1,14 @@
 class Solution:
-    def __init__(self):
-        self.memo = {0: 0, 1: 1, 2: 1}
-
     def tribonacci(self, n: int) -> int:
-        if n in self.memo:
-            return self.memo[n]
-        self.memo[n] = self.tribonacci(n - 3) + self.tribonacci(n - 2) + self.tribonacci(n - 1)
-        return self.memo[n]
+        
+        @cache
+        def dp(n: int) -> int:
+            if n == 0:
+                return 0
+            
+            if n in [1, 2]:
+                return 1
+            
+            return dp(n - 3) + dp(n - 2) + dp(n - 1)
+
+        return dp(n)

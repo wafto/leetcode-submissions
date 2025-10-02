c7621f6140cb35d2835f030f22e5707436192acf
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-10-02 02:46:48 +0000

    Code challenge submission for question: 55 Jump Game

--- 55 Jump Game/55jump-game.py
+++ 55 Jump Game/55jump-game.py
@@ -1,5 +1,6 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        """
         goal = len(nums) - 1
 
         for i in range(len(nums) - 2, -1, -1):
@@ -7,3 +8,38 @@ class Solution:
                 goal = i
 
         return goal == 0
+        """
+
+        """
+        @cache
+        def dp(i: int) -> bool:
+            if i >= len(nums) - 1:
+                return True
+
+            if nums[i] == 0:
+                return False
+
+            for j in range(i + 1, i + nums[i] + 1):
+                if dp(j):
+                    return True
+
+            return False
+
+        return dp(0)
+        """
+
+        n = len(nums)
+        dp = [False] * n
+        dp[-1] = True
+
+        for i in range(n - 2, -1, -1):
+            if i + nums[i] >= n:
+                dp[i] = True
+                continue
+            
+            for j in range(i + 1, i + nums[i] + 1):
+                if dp[j]: 
+                    dp[i] = True
+                    break
+                    
+        return dp[0]

568ef8a0e3b51b1e5297300971849b2111cfca9b
Aftab Hussain Miranda <wafto.mx@gmail.com>
2025-10-02 18:08:33 +0000

    Code challenge submission for question: 3100 Water Bottles II

---
+++ 3100 Water Bottles II/3100water-bottles-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        ans = numBottles
+        
+        while numBottles >= numExchange:
+            numBottles -= numExchange - 1
+            numExchange += 1
+            ans += 1
+        
+        return ans
+            
+
